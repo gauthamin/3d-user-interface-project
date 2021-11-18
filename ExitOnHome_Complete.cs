2263d3c4a69623e30e1ff1909de3968913a059fa
Gauthami.N <shlokanarendra@gmail.com>
2021-11-18 20:48:50 +0530

    Add files via upload

---
+++ ExitOnHome_Complete.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitOnHome_Complete : MonoBehaviour
+{
+    //Quits application or stops playmode in Unity Editor
+    public void ExitApp()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Exit to Home: Unity Editor Quit");
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ ShowUI_Complete.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MagicLeapTools;
+
+public class ShowUI_Complete : MonoBehaviour
+{ 
+    private PointerReceiver _pointer;
+
+    //Function to switch UI betwen enabled and disabled states
+    //while user is dragging parent object
+    public void EnableUI()
+    {
+        //Check if there is a reference to pointer receiver
+        if(_pointer == null)
+        {
+            //Get pointer receiver component from parent
+            _pointer = GetComponentInParent<PointerReceiver>();
+        }
+
+        //Check if the state of the pointer is in a dragging state
+        if (_pointer != null && _pointer.Dragging)
+        {
+            //Set game object active status to opposite of current status
+            gameObject.SetActive(!gameObject.activeSelf);
+        }       
+    }
+
+    //Disables UI regardless of what current state UI is in
+    public void DisableUI()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
